7cd1d39384310bc1fdf322e2f0bb9fb8fc427d89
TheJoaoVitorio <joaovtr999x@gmail.com>
2025-10-18 14:59:22 -0300

    Implement Iphone and Nokia classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone(numero: "(11) 98765-4321", modelo: "Modelo 1", imei: "111111111", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+iphone.ReceberLigacao();
+Console.WriteLine("");
+
+Nokia nk = new Nokia("(11)997129874", "Nokia G50", "2334567830495823", 256);
+nk.Ligar();
+nk.InstalarAplicativo("WhatsApp");
+nk.ReceberLigacao();
